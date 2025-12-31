6902ed4aa2197567051dfdd4cd75331eb344a7a3
Lance Nines <lance.nines@gmail.com>
2025-12-31 13:35:03 -0500

    add web/CLI mode switching and templ/chi dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ninesl/zombie-chickens
 
-go 1.25.4
+go 1.25.5
+
+require (
+	github.com/a-h/templ v0.3.960 // indirect
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+)
--- main.go
+++ main.go
@@ -2,64 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
 
+	"github.com/ninesl/zombie-chickens/cligame"
+	"github.com/ninesl/zombie-chickens/webapp"
 	"github.com/ninesl/zombie-chickens/zcgame"
 )
 
 func main() {
-	debug := flag.Bool("debug", false, "put events in top of night deck for testing")
+	web := flag.Bool("web", false, "run web server instead of CLI game")
+	debug := flag.Bool("debug", false, "put events on top of night deck for testing")
 	flag.Parse()
 
-	names := flag.Args()
-	if len(names) < 1 {
-		log.Fatal("usage: go run . [-debug] name1 [name2 ...]")
-	}
+	zcgame.DebugMode = *debug
 
-	zcgame.SetCLIMode(true)
-
-	game, err := zcgame.CreateNewGame(names...)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if *debug {
-		game.DebugEventsOnTop()
-	}
-
-	// Game loop with input handling
-	for {
-		// Try to advance the game
-		gameContinues, inputNeeded := game.ContinueDay()
-
-		if inputNeeded != nil {
-			// CLI mode: gather input and continue
-			if !zcgame.IsCLIMode() {
-				log.Fatal("Player input needed but not in CLI mode: ", inputNeeded.Message)
-			}
-
-			input := zcgame.GatherCLIInput(game, inputNeeded)
-
-			// Provide input and continue
-			for {
-				gameContinues, inputNeeded = game.ContinueAfterInput(input)
-				if inputNeeded == nil {
-					break
-				}
-				input = zcgame.GatherCLIInput(game, inputNeeded)
-			}
-		}
-		zcgame.RefreshRender(game)
-
-		if gameContinues {
-			// Day completed successfully, show state and continue
-			continue
-		}
-
-		// Game over - all players eliminated
-		fmt.Println("GAME OVER - All players have been eliminated!")
-		break
+	if *web {
+		webapp.RunServer()
+	} else {
+		cligame.RunGame()
 	}
 }
